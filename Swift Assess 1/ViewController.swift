2af9937b01318e578fb3373b236780420f08661f
David Gretz <dg@buckeyeguy.com>
2016-09-17 16:08:27 +0700

    Final commit

--- Swift Assess 1/ViewController.swift
+++ Swift Assess 1/ViewController.swift
@@ -1,10 +1,10 @@
-//
+/*
 //  ViewController.swift
 //  Swift Assess 1
 //
 //  Created by David Gretz on 9/17/16.
 //  Copyright © 2016 BadinHS. All rights reserved.
-//
+*/
 
 import UIKit
 
@@ -12,16 +12,25 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var myLabel2: UILabel!
     
+    var pressCount = 0
+    
+    @IBAction func countingButton(_ sender: AnyObject) {
+        pressCount = pressCount+1
+        
+        if pressCount == 10 {
+            self.view.backgroundColor=UIColor.gray
+        }
+        
+        if pressCount == 15 {
+            self.view.backgroundColor=UIColor.yellow
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         self.view.backgroundColor=UIColor.yellow
-        
-        
-        
-        
     }
 
     override func didReceiveMemoryWarning() {
